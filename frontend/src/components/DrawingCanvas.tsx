557dbc419b58ba87091632b36b2948cfc15ed315
Raon98 <cheol1998@kakao.com>
2024-03-21 16:43:18 +0900

    /*20240321 텍스트 필터기능 영어/한글변환 filter*/

--- frontend/src/components/DrawingCanvas.tsx
+++ frontend/src/components/DrawingCanvas.tsx
@@ -1,25 +1,22 @@
 import React, {useState, useRef, useEffect} from 'react';
 
 import CanvasDraw from "react-canvas-draw";
-import axios from 'axios';
 import {$api} from "plugins/api";
 
 interface DrawingCanvasProps {}
-
-const config = {
-    baseURL: 'http://localhost:8090',
-    timeout: 60 * 1000,
-    withCredentials: true
+enum Language {
+    ENGLISH = '영어(ENGLISH)',
+    KOREAN = '한글(KOREAN)'
 }
+
 const {AsyncPost} = $api
-const _axios = axios.create(config);
 
 const DrawingCanvas: React.FC<DrawingCanvasProps> = () => {
     const canvasRef = useRef<CanvasDraw>(null);
     const [recognizedText, setRecognizedText] = useState<string[]>([]);
-
+    const [lang, setLang] = useState<String>(Language.KOREAN)
     const func = {
-        handleCanvasDraw : ()=> {
+        handleCanvasDraw: () => {
             if (!canvasRef.current) return;
             // @ts-ignore
             const imageData = canvasRef.current.canvas.drawing.toDataURL('image/png').split(',')[1];
@@ -27,32 +24,34 @@ const DrawingCanvas: React.FC<DrawingCanvasProps> = () => {
 
             AsyncPost('ext', 'GoogleVision', 'DrawingCanvass', params,
                 (res) => {
-                    setRecognizedText(func.recognizedFilter([...res]))
+                    setRecognizedText(func.recognizedFilter([...res.data]))
                 });
+
+          // TEST :  func.recognizedFilter([...'김ㅅtㅣ씩시TA'])
         },
         clearCanvas:()=>{
             if (!canvasRef.current) return;
             canvasRef.current.clear();
         },
         /*20240321 텍스트 필터기능 모음자음일경우 filter*/
-        recognizedFilter:(textArr:string[])=>{
-            let arr = textArr.filter(v=>v !== '\n' && func.containsLetters(v))
+        recognizedFilter:(textArr:string[])=> {
+            let arr = textArr.filter(v => v !== '\n' && lang === Language.KOREAN ?
+                (/[ㄱ-ㅎㅏ-ㅣ가-힣]/.test(v) && func.containsLetters(v)) : /[a-zA-Z]/.test(v))
+
             return arr;
         },
         containsLetters:(str:string) => {
-            let word = [
-                {w1:'ㄱ',w2:'ㅎ'},
-                {w1:'ㅏ',w2:'ㅣ'}
-            ]
+            let word: { w1: string, w2: string }[] = [{w1: 'ㄱ', w2: 'ㅎ'}, {w1: 'ㅏ', w2: 'ㅣ'}]
+
             let result
-            for (let i=0; i<word.length; i++){
-                result = word.every( char => str <= word[i].w2 && word[i].w1 <= str)
-                if (result === true){
+            for (let i = 0; i < word.length; i++) {
+                result = word.every(char => str <= word[i].w2 && word[i].w1 <= str)
+                if (result === true) {
                     return false
                 }
             }
             return true
-    }
+        }
     }
     useEffect(()=>{
         if(recognizedText.length > 0) {
@@ -74,7 +73,11 @@ const DrawingCanvas: React.FC<DrawingCanvasProps> = () => {
             />
             <button onClick={func.handleCanvasDraw}>Recognize Text</button>
             <button onClick={func.clearCanvas}>Clear</button>
-            <div style={{ fontSize : 30 }}>추출한 텍스트입니다.: {recognizedText}</div>
+            <div style={{fontSize: 30}}>현재 언어 : {lang}</div>
+            <button
+                onClick={() => lang === Language.KOREAN ? setLang(Language.ENGLISH) : setLang(Language.KOREAN)}>언어변경
+            </button>
+            <div style={{fontSize: 30}}>추출한 텍스트입니다.: {recognizedText}</div>
         </div>
     );
 };
