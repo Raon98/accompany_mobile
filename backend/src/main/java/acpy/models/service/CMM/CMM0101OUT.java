788ad607537446e97fd55c21e0710a97e4e9b74a
Raon98 <cheol1998@kakao.com>
2024-04-04 09:42:58 +0900

    backend 라우터 파일 원복

---
+++ backend/src/main/java/acpy/models/service/CMM/CMM0101OUT.java
@@ -0,0 +1,14 @@
+package acpy.models.service.CMM;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class CMM0101OUT {
+
+   private Object data;
+   private String code;
+   private String msg;
+
+}
