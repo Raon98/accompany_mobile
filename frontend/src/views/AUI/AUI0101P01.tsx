2ae9603d3ba96ce354185f81098b5e7d5c18d47c
Raon98 <cheol1998@kakao.com>
2024-04-09 17:08:52 +0900

    파일임시저장

--- frontend/src/views/AUI/AUI0101P01.tsx
+++ frontend/src/views/AUI/AUI0101P01.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 
 const AUI0101P01 = () => {
+    const navigate = useNavigate();
+    console.log("뭐지")
     return (
         <div>
             로그인페이지
+           <div>
+               <button onClick={() => navigate("/ACM0101P01")}>클릭하세요</button>
+           </div>
         </div>
     );
 };
