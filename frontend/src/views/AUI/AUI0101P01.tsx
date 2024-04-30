f177316e28eb492bf0a7f01d0a965a09436a26f3
Raon98 <cheol1998@kakao.com>
2024-04-30 15:13:59 +0900

    20240430 frontend docker 실시간 연동 성공

--- frontend/src/views/AUI/AUI0101P01.tsx
+++ frontend/src/views/AUI/AUI0101P01.tsx
@@ -2,10 +2,10 @@ import { useNavigate } from "react-router-dom";
 
 const AUI0101P01 = () => {
     const navigate = useNavigate();
-    console.log("왜안돼12324")
+    console.log("왜안돼121")
     return (
         <div>
-            로그인페이지 실시간반영됐나? 지금은 왜안돼
+            로그인페이지 실시간반영됐나?
            <div>
                <button onClick={() => navigate("/ACM0101P01")}>클릭하세요</button>
            </div>
