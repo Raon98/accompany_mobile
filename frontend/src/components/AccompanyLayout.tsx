be9f84916ff2832ff4e26eecb01629fd1603e775
Raon98 <cheol1998@kakao.com>
2024-05-10 16:41:38 +0900

    라우터 관리방법 수정

--- frontend/src/components/AccompanyLayout.tsx
+++ frontend/src/components/AccompanyLayout.tsx
@@ -1,6 +1,8 @@
-import React, {lazy, useState} from 'react';
+import React, {lazy, useEffect, useState} from 'react';
 import AccompanySection from "components/AccompanySection";
 import Router from "router";
+import {$api} from "plugins/api";
+import {useLocation} from "react-router-dom";
 
 
 const AccompanyHeader = lazy(() => import("components/AccompanyHeader"));
@@ -12,29 +14,29 @@ const AccompanyLayout = () => {
     const role = 'C,A'
     const [headerFlag, setHeaderFlag] = useState(true)
     const [footerFlag, setFooterFlag] = useState(true)
+    const location = useLocation()
 
+    useEffect(() => {
+        /*20240327 헤더푸터 사용/미사용여부 체크*/
+        // if (pathList?.length) {
+        //     setHeaderFlag(pathList.filter(v => v.path === location.pathname)[0].hd_flag === 'Y')
+        //     setFooterFlag(pathList.filter(v => v.path === location.pathname)[0].ft_flag === 'Y')
+        // }
 
-    // useEffect(() => {
-    //     /*20240327 헤더푸터 사용/미사용여부 체크*/
-    //     // if (pathList?.length) {
-    //     //     setHeaderFlag(pathList.filter(v => v.path === location.pathname)[0].hd_flag === 'Y')
-    //     //     setFooterFlag(pathList.filter(v => v.path === location.pathname)[0].ft_flag === 'Y')
-    //     // }
-    //
-    //     console.log(location.pathname)
-    //     $api.AsyncPost('api', 'ACS0101S01', '', {path : location.pathname}, (res) => {
-    //
-    //         console.log(res.data)
-    //         // let pathList = routeFilter.map((v: RspProps) => ({
-    //         //     path: v.path,
-    //         //     component: lazy(() => import(`${v.component}`)),
-    //         //     hd_flag : v.hd_flag,
-    //         //     ft_flag : v.ft_flag,
-    //         //     auth_yn : v.auth_yn
-    //         // }))
-    //     });
-    //
-    // }, [location.pathname])
+        console.log(location.pathname)
+        $api.AsyncPost('api', 'ACS0101S01', '', {path : location.pathname}, (res) => {
+
+            console.log(res.data)
+            // let pathList = routeFilter.map((v: RspProps) => ({
+            //     path: v.path,
+            //     component: lazy(() => import(`${v.component}`)),
+            //     hd_flag : v.hd_flag,
+            //     ft_flag : v.ft_flag,
+            //     auth_yn : v.auth_yn
+            // }))
+        });
+
+    }, [location.pathname])
 
     return (
         <>
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -4,36 +4,75 @@ import ACC0101P01 from "views/ACC/ACC0101P01";
 import AAC0101P01 from "views/AAC/AAC0101P01";
 import ACC0102P01 from "views/ACC/ACC0102P01";
 import ACC0102P02 from "views/ACC/ACC0102P02";
+import ACC0101P02 from "views/ACC/ACC0101P02";
 
 interface RouterProps {
     isAuthenticated: boolean;
     role?: string;
 }
 
-export default function Router({ isAuthenticated, role }: RouterProps) {
+/* 로그인 전 공통 */
+const NCM = [
+    {
+        path: "/ACC0102P01",
+        el: <ACC0102P01/>
+    },
+    {
+        path: "/ACC0102P02",
+        el: <ACC0102P02/>
+    }
+]
+/* 로그인 후 공통  */
+const YCM = [
+    {
+        path: "/ACC0101P02",
+        el: <ACC0101P02/>
+    }
+]
+/*관리자 */
+const ADM = [
+    {
+        path: "/AAC0101P01",
+        el: <AAC0101P01/>
+    }
+]
+
+/*라우터 통합 관리 (Router integrated management)*/
+export const RIM = [...NCM, ...YCM, ...ADM]
+
+export default function Router({isAuthenticated, role}: RouterProps) {
     return (
         <Routes>
+            {/* 로그인 전 경로 */}
+            {!isAuthenticated && (
+                <>
+                    {NCM.map(route => (
+                        <Route path={route.path} element={route.el}/>
+                    ))}
+                    <Route path="/*" element={<ACC0102P01/>}/>
+                </>
+            )}
             {/* 로그인 후 공통 경로 */}
             {isAuthenticated && (
                 <>
-                    <Route path="/" element={<ACC0101P01 />} />
-                    <Route path="*" element={<ACC0101P01 />} />
+                    {YCM.map(route => (
+                        <Route path={route.path} element={route.el}/>
+                    ))}
+                    <Route path="/*" element={<ACC0101P01/>}/>
                 </>
             )}
 
             {/* 관리자 권환 */}
             {isAuthenticated && role === 'A' && (
-                <Route path="/AAC0101P01" element={<AAC0101P01 />} />
-            )}
-
-            {/* 로그인 전 경로 */}
-            {!isAuthenticated && (
                 <>
-                    <Route path="/ACC0102P01" element={<ACC0102P01 />} />
-                    <Route path="/ACC0102P02" element={<ACC0102P02 />} />
-                    <Route path="/*" element={<ACC0102P01 />} />
+                    {ADM.map(route => (
+                        <Route path={route.path} element={route.el}/>
+                    ))}
+                    <Route path="/*" element={<ACC0101P01/>}/>
                 </>
             )}
+
+
         </Routes>
     );
 }
