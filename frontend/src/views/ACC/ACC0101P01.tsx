11ffa589caf2c7393b23dac21780f3639b9d1d5e
Raon98 <cheol1998@kakao.com>
2024-05-11 12:09:37 +0530

    test

--- frontend/src/views/ACC/ACC0101P01.tsx
+++ frontend/src/views/ACC/ACC0101P01.tsx
@@ -11,7 +11,7 @@ const ACC0101P01 = () => {
     const navigate = useNavigate();
     return (
         <div>
-            메인페이지입니다 맥 깃허브 푸시테스트
+            메인페이지입니다 맥 깃허브 푸시테스트 tes
         </div>
     );
 };
