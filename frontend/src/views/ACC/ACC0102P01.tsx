02b04f24f497a5ebf2390f8c7d71bf8daaa082d9
Raon98 <cheol1998@kakao.com>
2024-05-17 15:46:43 +0900

    로그인페이지 scss 작업

--- frontend/src/views/ACC/ACC0102P01.tsx
+++ frontend/src/views/ACC/ACC0102P01.tsx
@@ -1,16 +1,15 @@
- import React from 'react';
-import {useNavigate} from "react-router-dom";
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 
- /******************************
-  * @공통 (ACCOMPANY COOMON COMPONENT)
-  * @화면명:로그인화면
-  * @작성자:김성철
-  ********************************/
+/******************************
+ * @공통 (ACCOMPANY COMMON COMPONENT)
+ * @화면명:로그인화면
+ * @작성자:김성철
+ ********************************/
 
 const ACC0102P01 = () => {
     const navigate = useNavigate();
 
-    console.log("로그인페이지")
     return (
         <div className="login">
             <div className="login__logo">
@@ -19,21 +18,21 @@ const ACC0102P01 = () => {
             <fieldset className="form">
                 <dl className="form__icon id">
                     <dd>
-                        <div className="from__block">
-                            <input type="text" name="username" title="아이디" placeholder="아이디를 입력해주세요."/>
+                        <div className="form__block">
+                            <input type="text" name="username" title="아이디" placeholder="아이디를 입력해주세요." />
                         </div>
                     </dd>
                 </dl>
                 <dl className="form__icon password">
                     <dd>
-                        <div className="from__block">
-                            <input type="text" name="password" title="비밀번호" placeholder="비밀번호를 입력해주세요."/>
-                            <button type="button" name="password" className="btn-password"/>
+                        <div className="form__block">
+                            <input type="password" name="password" title="비밀번호" placeholder="비밀번호를 입력해주세요." />
+                            <button type="button" name="password" className="btn-password" />
                         </div>
                     </dd>
                 </dl>
-                <div className="form__util">
-                    <a href="javascript:void(0)" className="from__sign">
+                <div className="form__utils">
+                    <a href="javascript:void(0)" className="form__sign">
                         회원가입
                     </a>
                 </div>
