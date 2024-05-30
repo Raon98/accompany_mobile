6b53d76260131d2e86ac177982c2621c738d43d4
Raon98 <cheol1998@kakao.com>
2024-05-30 13:13:09 +0900

    react_query 추가

--- _frontend/src/App.tsx
+++ _frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import {ToastContainer} from "react-toastify";
 import AccompanyLayout from "components/AccompanyLayout";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient()
 
 const App = () => {
     return (
 
-            <>
+        <>
+            <QueryClientProvider client={queryClient}>
                 <ToastContainer/>
                 <AccompanyLayout/>
-            </>
+                <ReactQueryDevtools initialIsOpen={true} />
+            </QueryClientProvider>
+        </>
 
     );
 };
