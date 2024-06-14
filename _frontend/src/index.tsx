779f70c0835b88812b8e436bed050085859948a3
Raon98 <cheol1998@kakao.com>
2024-06-14 17:50:39 +0900

    feat.font 외부 URL방식에서 내부방식으로 변경

--- _frontend/src/index.tsx
+++ _frontend/src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "App";
 import {BrowserRouter} from "react-router-dom";
-import "./assets/scss/main.scss"
+import "./assets/scss/main.scss";
+import "./assets/scss/themes/_fonts.scss";
 
 const root = ReactDOM.createRoot(
     document.getElementById('app') as HTMLElement
