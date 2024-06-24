483b7ed5262ee55907ee8d895464c97f3e57f28d
Raon98 <cheol1998@kakao.com>
2024-06-24 15:49:49 +0900

    feat: 암호화 복호화방식 수정

--- backend/src/main/java/acpy/api/utils/AesUtil.java
+++ backend/src/main/java/acpy/api/utils/AesUtil.java
@@ -1,45 +1,40 @@
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.IvParamterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
+import org.apache.commons.codes.binary.Hex;
 
 public class AesUtil {
-
-
-    private static final String ALGORITHM = "AES";
-    private static final int KEY_SIZE = 128;
-    private static byte[] keyValue;
 
     @Autowired
     private static AppConfig appConfig;
 
-    static {
-        try {
-            keyValue = appConfig.getPwdPrivateKey().getBytes();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    // private static byte[] privateKey_256 = appConfig.getPwdPrivateKey();
+    private static byte[] privateKey_256 = 'a1c2c3o4m5p6a7n8y9a1e0sprivatekey'
+    
 
-    public static String encrypt(String valueToEnc) throws Exception {
-        KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
-        keyGen.init(KEY_SIZE);
-        SecretKey secretKey = new SecretKeySpec(keyValue, ALGORITHM);
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        byte[] encryptedValue = cipher.doFinal(valueToEnc.getBytes());
-        return Base64.getEncoder().encodeToString(encryptedValue);
+    public static String encrypt(String plainText) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(privateKey_256.getBytes("UTF-8"),"AES");
+        IvParamterSpec IV = new IvParamterSpec(privateKey_256.substring(0,16).getBytes());
+
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE,secretKey,IV);
+
+        byte[] encryptionByte = c.doFinal(plainText)
+
+        return Hex.encodeHexString(encryptionByte);
     }
 
     public static String decrypt(String encryptedValue) throws Exception {
-        KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
-        keyGen.init(KEY_SIZE);
-        SecretKey secretKey = new SecretKeySpec(keyValue, ALGORITHM);
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        byte[] decodedValue = Base64.getDecoder().decode(encryptedValue);
-        byte[] decryptedValue = cipher.doFinal(decodedValue);
-        return new String(decryptedValue);
+        SecretKeySpec secretKey = new SecretKeySpec(privateKey_256.getBytes("UTF-8"),"AES");
+        IvParamterSpec IV = new IvParamterSpec(privateKey_256.substring(0,16).getBytes());
+
+        Cipher c = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		c.init(Cipher.DECRYPT_MODE, secretKey,IV);
+		byte[] decodeByte = Hex.decodeHex(encodeText.toCharArray());
+		
+		return new String(c.doFinal(decodeByte), "UTF-8");
     }
 }
