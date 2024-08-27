1ab021bcfb445e8784d660ae7a7951d8a35eac1f
Raon98 <cheol1998@kakao.com>
2024-08-27 13:29:23 +0900

    feat : User정보 store 추가 및 storage 추가

---
+++ _frontend/src/components/utils/store/Storage.ts
@@ -0,0 +1,21 @@
+export const localStorageEffect = (key:string) => ({setSelf,onSet}:any) => {
+    const saveItem = localStorage.getItem(key);
+    if(saveItem !=null){
+        setSelf(JSON.parse(saveItem))
+    }
+
+    onSet((newItem:any) => {
+        localStorage.setItem(key,JSON.stringify(newItem))
+    })
+}
+
+export const sessionStorageEffect = (key: string) => ({ setSelf, onSet }: any) => {
+    const savedItem = sessionStorage.getItem(key);
+    if (savedItem != null) {
+        setSelf(JSON.parse(savedItem));
+    }
+
+    onSet((newItem: any) => {
+        sessionStorage.setItem(key, JSON.stringify(newItem));
+    });
+};
---
+++ _frontend/src/state/useUser.ts
@@ -0,0 +1,22 @@
+import { useRecoilState } from "recoil";
+import { User, userInfo } from "store/useUserStore";
+
+
+interface UseUser {
+    user : User
+    setUserInfo : (user:User) => void
+}
+
+const useUser = (): UseUser => {
+  const [user, setUser] = useRecoilState(userInfo);
+
+  const setUserInfo = (user : User) => {
+        setUser(user)
+  }
+  return {
+        user,
+        setUserInfo
+  };
+};
+
+export default useUser;
