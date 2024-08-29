ea8c8b61e95120aaeb4051411729cd526434e08a
Raon98 <cheol1998@kakao.com>
2024-08-29 16:52:06 +0900

    feat : 메인페이지 결혼 슬라이드 퍼블완성

--- _frontend/src/views/main/ACM0101P01.tsx
+++ _frontend/src/views/main/ACM0101P01.tsx
@@ -15,7 +15,18 @@ const ACM0101P01 = () => {
     <div className="main">
       <div className="main__top">
         <div className="top__block">
-          <div className="logo"></div>
+          <div className="logo">
+          <img
+                  src={require(`assets/images/noti.png`)}
+                  alt="arrow"
+                  style={{ height: "1.75rem" }}
+                ></img>
+                <img
+                  src={require(`assets/images/logout.png`)}
+                  alt="arrow"
+                  style={{ height: "1.75rem" }}
+                ></img>
+          </div>
           <div className="option">
             <div className="setting"></div>
             <div className="logout"></div>
@@ -34,29 +45,35 @@ const ACM0101P01 = () => {
       <div className="main__container">
         <div className="schedule">
           <div className="schedule__title">동행일정</div>
-          <div
-            className="schedule__contents"
-            style={{ "--schedule-border": "#f57676" } as React.CSSProperties}
-          >
+          <div className="schedule__contents marry">
             <div className="schedule__item">
               <div className="item__top-content">
                 <div className="item__title">
-                  <span className="name">임정훈</span>님의{" "}
+                  <span className="name">임정훈</span>
+                  <span>님의 </span>
                   <span className="event">결혼</span>
                 </div>
                 <img
                   src={require(`assets/images/rightArrow_gray.png`)}
                   alt="arrow"
-                  style={{height:"0.85rem"}}
+                  style={{ height: "0.85rem" }}
                 ></img>
               </div>
-              <div
-                className="item__content marry"
-                style={
-                  { "--schedule-border": "#f89c9c" } as React.CSSProperties
-                }
-              >
-                <div className="content__title"></div>
+              <div className="item__content">
+                <div className="date__block">
+                  <div className="dDay">D-50</div>
+                  <div className="date">2025-03-16</div>
+                </div>
+                <div className="contents__block">
+                  <div className="family">
+                    <div>임홍규·이미진</div>
+                    <div className="children">자녀 임정훈</div>
+                  </div>
+                  <div className="family">
+                    <div>이노혁·박미주</div> 
+                    <div className="children">지녀 이유리</div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
